9be6a563aadda9aa06356b2c3acc7f380f9417be
MODeVo1994 <mm7141331@gmail.com>
2023-04-01 20:32:07 +0200

    all changes7

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,18 @@
+ 
+import './App.scss'
+import Navbar from './components/navbar/Navbar'
+import React from 'react'
+
+function App() {
+ 
+  return (
+    <div>
+     <p>kkk</p>
+       <h1>hello</h1>
+       <Navbar/>
+ 
+    </div>
+  )
+}
+
+export default App
